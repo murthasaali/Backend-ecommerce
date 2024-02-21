6dd338ae362f2bb29051808331f997f3be61e0d2
murthasaali <murthasaalick123@gmail.com>
2024-02-21 21:29:03 +0530

    erferf

--- Controllers/postController.js
+++ Controllers/postController.js
@@ -21,26 +21,26 @@ const postController = {
   },
   commentPost: async (req, res) => {
     try {
-      const  postId  = req.params.postId; // Assuming postId is passed in the URL params
+      const postId = req.params.postId; // Assuming postId is passed in the URL params
       const { text, author } = req.body; // Assuming author (userId) is passed in the request body
-console.log(postId)
+  
       // Check if the post exists
       const post = await PostSchema.findById(postId);
       if (!post) {
         return res.status(404).json({ error: 'Post not found' });
       }
-
+  
       // Create new comment
       const newComment = await CommentSchema.create({
         post: postId,
         text,
         author // Assuming author is the userId
       });
-
+  
       // Add the comment to the post
       post.comments.push(newComment._id);
       await post.save();
-
+  
       // Return success response
       return res.status(201).json({ message: 'Comment added successfully', comment: newComment });
     } catch (error) {
@@ -48,6 +48,7 @@ console.log(postId)
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   }
+  
 };
 
 module.exports = postController;
