59cd28982c9a62732b57c352d63ab023f6108515
murthasaali <murthasaalick123@gmail.com>
2024-03-28 00:47:29 +0530

    url updated

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ const server = http.createServer(app);
 // Initialize Socket.IO
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000", // Allow requests from this origin
+    origin: "https://unity-swart.vercel.app", // Allow requests from this origin
     methods: ["GET", "POST"]
   }
 });
