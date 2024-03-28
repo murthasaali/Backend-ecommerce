25139431a76b26e5097ce394e58ced8e8f31a3c4
murthasaali <murthasaalick123@gmail.com>
2024-03-28 13:32:26 +0530

    cors updated

--- app.js
+++ app.js
@@ -29,10 +29,7 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors({
-  origin: "https://unity-swart.vercel.app",
-  methods: ["GET", "POST"],
-}));
+app.use(cors());
 
 // Connect to MongoDB using Mongoose
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
