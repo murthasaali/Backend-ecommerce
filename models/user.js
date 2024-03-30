32d67a1a3a3ae7009537f69a5a8bd4d42592bb5e
murthasaali <murthasaalick123@gmail.com>
2024-03-30 17:45:49 +0530

    user modal updated

--- models/user.js
+++ models/user.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
+const followerSchema = new Schema({
+  followerId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
+});
 
 const userSchema = new Schema(
   {
@@ -25,16 +34,10 @@ const userSchema = new Schema(
       type: String,
     },
     followers: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-      },
+   followerSchema
     ], // Array of followers
     following: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-      },
+    followerSchema
     ],
     posts: [
       {
